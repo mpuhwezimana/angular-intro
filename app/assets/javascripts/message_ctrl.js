95619ca6b5c0ab2ff81c23a6e0c180db53a99a98
mpuhwezimana <mpuhwearsene.com>
2017-02-28 14:17:13 +0200

    continued working on the to do app

--- app/assets/javascripts/message_ctrl.js
+++ app/assets/javascripts/message_ctrl.js
@@ -5,8 +5,12 @@
 
     $scope.tasks = ["take out the trash", "walk the dog", "play soccer", "wash the clothes"];
 
+    $scope.addTask = function(task) {
+      $scope.tasks.push(task);
+      $scope.newTask = null;
 
+    };
 
-    window.$scope = $scope;
-  });
+  window.$scope = $scope;
+});
 })();
